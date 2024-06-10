1c94ebcd0f389325728713be00c67a6c836e4713
Thomas Scheepers <60781257+Knetters@users.noreply.github.com>
2024-06-10 08:58:53 +0200

    Changed port

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import styles from "./styles/Home.module.css";
 let query: string;
 let locale: string;
 let pageSize: number;
-query = "shirts";
+query = "bags";
 locale = "nl-NL";
 pageSize = 25;
 
